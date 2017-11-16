4aa6da7c08852b8f0bfc204b17412831b7d5a25f
Darnell W <darnellwillis96@gmail.com>
2017-11-16 00:38:03 -0700

    additional files

--- server.js
+++ server.js
@@ -14,8 +14,45 @@ let Schema = mongoose.Schema;
 
 // Initialize Express
 let app = express();
+app.use(express.static("public"));
+
+
+// Database configuration
+// Save the URL of our database as well as the name of our collection
+var databaseUrl = "zoo";
+var collections = ["animals"];
+
+// Use mongojs to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+
+// Routes
+// 1. At the root path, send a simple hello world message to the browser
+app.get("/", function(req, res) {
+    res.sendFile("index.html");
+  });
+
+
+app.get("/all", function(req, res) {
+    // Query: In our database, go to the animals collection, then "find" everything
+    db.animals.find({}, function(error, found) {
+      // Log any errors if the server encounters one
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
+        res.json(found);
+      }
+    });
+  });
 
 
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+    console.log("App running on port " + PORT);
   });
