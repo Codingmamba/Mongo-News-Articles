1f6093676e389ca0b0580902c0ea435fac4f5fff
Darnell W <darnellwillis96@gmail.com>
2017-11-23 17:50:01 -0700

    Updated routes

---
+++ public/assets/cherrio.js
@@ -0,0 +1,35 @@
+let cheerio = require("cheerio");
+let request = require("request");
+let mongoose = require("mongoose");
+
+
+$("#scrape").on("click", function() {
+  request('https://www.nytimes.com/section/us?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=U.S.&WT.nav=page', function (error, response, html) {
+  let $ = cheerio.load(html);
+  let results = [];
+  // $('p.title').each(function(i, element){
+
+  //     let title = $(this).text();
+  //     let link = $(element).children().attr('href');
+
+  //     result.push({
+  //       title:title,
+  //       link:link
+  //     });
+
+      $("h2.headline").each(function(i, element) {
+          // Save the text of the h4-tag as "title"
+          var title = $(this).text();
+          // Find the h4 tag's parent a-tag, and save it's href value as "link"
+          var link = $(element).children().attr("href");
+          // Make an object with data we scraped for this h4 and push it to the results array
+          results.push({
+            title: title,
+            link: link
+          });
+
+ $("#titles").text(results);
+});
+  });
+  res.send("Scrape Complete");
+});
--- server.js
+++ server.js
@@ -7,8 +7,13 @@ let express = require("express");
 let mongojs = require("mongojs");
 let bodyParser = require("body-parser");
 let logger = require("morgan");
+let path = require("path");
+let js = require('./public/assets/cherrio.js');
 
-let PORT = 3000;
+let Note = require('./models/note.js');
+let Article = require('./models/article.js');
+
+var PORT = process.env.PORT || 3005;
 let Schema = mongoose.Schema;
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -18,28 +23,99 @@ let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 let app = express();
 app.use(express.static("public"));
 
+let db = mongoose.connection;
+
+db.on('error', function (err) {
+console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+console.log('Mongoose connection successful.');
+});
+
+
 // Routes
-// 1. At the root path, send a simple hello world message to the browser
-app.get("/", function(req, res) {
-    res.sendFile("index.html");
+app.get('/', function(req, res) {
+  res.send(index.html);
+});
+
+
+app.get('/scrape', function(req, res) {
+	request('https://www.nytimes.com/section/us?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=U.S.&WT.nav=page', function (error, response, html) {
+  let $ = cheerio.load(html);
+  let results = [];
+  // $('p.title').each(function(i, element){
+
+  //     let title = $(this).text();
+  //     let link = $(element).children().attr('href');
+
+  //     result.push({
+  //       title:title,
+  //       link:link
+  //     });
+
+      $("h2.headline").each(function(i, element) {
+          // Save the text of the h4-tag as "title"
+          var title = $(this).text();
+          // Find the h4 tag's parent a-tag, and save it's href value as "link"
+          var link = $(element).children().attr("href");
+          // Make an object with data we scraped for this h4 and push it to the results array
+          results.push({
+            title: title,
+            link: link
+          });
+
+  console.log(results);
+});
   });
+  res.send("Scrape Complete");
+});
 
 
-app.get("/all", function(req, res) {
-    // Query: In our database, go to the animals collection, then "find" everything
-    db.animals.find({}, function(error, found) {
-      // Log any errors if the server encounters one
-      if (error) {
-        console.log(error);
-      }
-      // Otherwise, send the result of this query to the browser
-      else {
-        res.json(found);
-      }
-    });
-  });
+app.get('/articles', function(req, res){
+	Article.find({}, function(err, doc){
+		if (err){
+			console.log(err);
+		} else {
+			res.json(doc);
+		}
+	});
+});
 
 
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT);
-  });
+app.get('/articles/:id', function(req, res){
+	Article.findOne({'_id': req.params.id})
+	.populate('note')
+	.exec(function(err, doc){
+		if (err){
+			console.log(err);
+		} else {
+			res.json(doc);
+		}
+	});
+});
+
+
+app.post('/articles/:id', function(req, res){
+	let newNote = new Note(req.body);
+
+	newNote.save(function(err, doc){
+		if(err){
+			console.log(err);
+		} else {
+			Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
+			.exec(function(err, doc){
+				if (err){
+					console.log(err);
+				} else {
+					res.send(doc);
+				}
+			});
+
+		}
+	});
+});
+
+app.listen(PORT , function() {
+  console.log('App running on port:'+ PORT);
+});
